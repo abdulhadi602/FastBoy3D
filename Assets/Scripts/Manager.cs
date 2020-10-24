91ce4f4fb95a6adbd8a2d68b95dcaaa36119da86
abdulhadi602 <ahadi3608@gmail.com>
2020-10-24 00:36:49 +0000

    Tips added

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
-    public  GameObject GameOverCanvas,GameStartCanvas;
-  
+    public  GameObject GameOverCanvas,GameStartCanvas,TipsCanvas;
+    public GameObject[] tips;
     private void Start()
     {
+        
         GameStartCanvas.SetActive(true);
       
         Time.timeScale = 0;
@@ -34,5 +35,29 @@ public class Manager : MonoBehaviour
  
         GameStartCanvas.SetActive(false);
         Time.timeScale = 1;
+    }
+    public void GetNextTips()
+    {
+        for (int i = 0; i<tips.Length; i++)
+        {
+            if (tips[i].activeSelf)
+            {
+                tips[i].SetActive(false);
+                if (i + 1 < tips.Length)
+                {
+                    tips[i + 1].SetActive(true);
+                }
+                else
+                {
+                    tips[0].SetActive(true);
+                }
+                break;
+            }
+           
+        }
+    }
+    public void SkipTips()
+    {
+        TipsCanvas.SetActive(false);
     }
 }
